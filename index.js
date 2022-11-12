982547a915f15fffe30c038bc3ac0b9aa06759b8
fitdizzle <markfitnessaz@gmail.com>
2022-11-12 12:40:34 -0700

    server b

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.get('/', (req, res) => {
-  res.send('Server A');
+  res.send('Server B');
 });
 
 app.listen(8080, () => {
-  console.log('server A running on port 8080');
+  console.log('server B running on port 8080');
 });
