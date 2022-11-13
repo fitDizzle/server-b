66eaf535b5a39fa4a8f7e1cb014cbac318aa4e70
fitdizzle <markfitnessaz@gmail.com>
2022-11-12 17:58:12 -0700

    pushing log for testing

--- server/controllers/productsController.js
+++ server/controllers/productsController.js
@@ -4,6 +4,7 @@ const source = process.env.DATABASE || 'attelier-product-db';
 
 module.exports = {
   getAllProducts: async (req, res) => {
+    console.log('GET ALL PRODUCTS ROUTE HIT');
     const client = new MongoClient(uri, { useUnifiedTopology: true });
     try {
       let products = await client.db(source).collection('products').find({}).toArray();
